f5c0c8f1bc30c912eaeca6de1f8383a4833e0ca5
Jihee Shin <jiheenim27@gmail.com>
2022-07-06 18:01:59 +0900

    get&delete 구현

--- To-do List/script.js
+++ To-do List/script.js
@@ -6,6 +6,12 @@
   }
 
   const $todos = get('.todos');
+  const $todoForm = get('.todo_form');
+  const $todoInput = get('.todo_input');
+  const $submitButton = get('.todo_submit_button');
+  const $contentButtons = get('.content_buttons');
+  const $editButtons = get('.edit_buttons');
+  const $editButton = get('.todo_edit_button');
 
   const createTodoElement = (item) => {
     const { id, content } = item
@@ -41,8 +47,65 @@
     return $todoItem
   }
 
+  // 모든 todo 가져오고 렌더링
+  const getTodos = () => {
+    fetch("http://localhost:1234/todos")
+      .then((res) => res.json())
+      .then((todos) => {renderAllTodos(todos)})
+  }
+
+  const renderAllTodos = (todos) => {
+    $todos.innerHTML = '';
+    console.log(todos);
+    todos.forEach( todo => {
+      $todos.appendChild(createTodoElement(todo));
+    });
+  }
+
+  // 입력한 todo post
+  const postTodo = (event) => {
+    event.preventDefault();
+    try {
+      const content = $todoInput.value;
+      const todo = {
+        "content": content
+      };
+      fetch("http://localhost:1234/todos", {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(todo)
+      })
+        .then((res) => res.json())
+        .then(getTodos)
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const deleteTodo = (event) => {
+    // event.preventDefault();
+    if(event.target.className === "todo_remove_button") {
+      const $item = event.target.closest('.item');
+      const $id = $item.dataset.id;
+      fetch(`http://localhost:1234/todos/${$id}`, {
+        method: 'DELETE',
+      })
+        .then((res) => console.log(res))
+    }
+  }
+
+  const modifyTodo = () => {
+    
+  }
+
   const init = () => {
-    $todos.appendChild(createTodoElement({"id": 1, "content": 'hahaha'}));
+    // getTodos();
+    getTodos();
+    $submitButton.addEventListener('click', postTodo);
+    $todos.addEventListener('click', deleteTodo);
+    $todos.addEventListener('click', modifyTodo);
   }
   init()
 })()
