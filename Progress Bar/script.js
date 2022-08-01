2ff49606d9e563f73f09a78540a9f07fed2e0262
Jihee Shin <jiheenim27@gmail.com>
2022-08-01 14:54:27 +0900

    Update script.js

--- Progress Bar/script.js
+++ Progress Bar/script.js
@@ -1,37 +1,32 @@
 ;(function () {
   'use strict'
 
-  let timerId;
-  
+  let timerId
+
   const get = (target) => {
     return document.querySelector(target)
   }
 
-  const throttle = (callback, time) => {
-    if (timerId) return;
-    timerId = setTimeout(() => {
-      callback();
-      timerId = undefined;
-    }, time);
-    
-  }
+  const $progressBar = get('.progress-bar')
 
-  const $progressBar = get('.progress-bar');
+  const throttle = (callback, time) => {
+    if (timerId) return
+    timerId = setTimeout(() => {
+      callback()
+      timerId = undefined
+    }, time)
+  }
 
   const onScroll = () => {
-    const height = 
-    document.documentElement.scrollHeight - 
-    document.documentElement.clientHeight;
-
-    const scrollTop = document.documentElement.scrollTop;
-
-    const width = (scrollTop / height) * 100;
-    $progressBar.style.width = width + '%';
-
-    console.log('width', width);
+    const scrollTop = document.documentElement.scrollTop
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight
+    const scrollWidth = (scrollTop / height) * 100
+    $progressBar.style.width = scrollWidth + '%'
   }
 
-  window.addEventListener('scroll', () => throttle(onScroll, 100));
-
-
+  window.addEventListener('scroll', () => {
+    throttle(onScroll, 100)
+  })
 })()
